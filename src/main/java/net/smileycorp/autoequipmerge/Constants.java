e005b8896552cb97944957f2cdf09c532dacd121
Smileycorp <callum.j.longbottom@gmail.com>
2024-10-30 03:24:58 +0000

    update versioning because I actually tested and I'm hoping I didn't goob again

--- src/main/java/net/smileycorp/autoequipmerge/Constants.java
+++ src/main/java/net/smileycorp/autoequipmerge/Constants.java
@@ -4,6 +4,6 @@ public class Constants {
 	
 	public static final String NAME = "Auto Equipment Merge";
 	public static final String MODID = "autoequipmerge";
-	public static final String VERSION = "beta-0.1.0";
+	public static final String VERSION = "1.0.0";
 	
 }
